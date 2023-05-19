08fcf93273c4b3169eba6fee6c919c648c24bdac
User <lonzo.trans@gmail.com>
2023-05-19 10:45:29 +0300

    autoservice

--- autoservice/urls.py
+++ autoservice/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('task_1/', views.task_1, name='task_1'),
+    path('task_2/', views.task_2, name='task_2'),
+    path('task_3/', views.task_3, name='task_3'),
+    path('task_4/', views.task_4, name='task_4'),
 ]
--- autoservice/views.py
+++ autoservice/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
     return render(request, 'autoservice/index.html')
+
+def task_1(request):
+    obj = Clients.objects.all()
+    return render(request, 'autoservice/task_1.html', {{'obj': obj}})
+
+def task_2(request):
+    return render(request, 'autoservice/task_2.html')
+
+def task_3(request):
+    return render(request, 'autoservice/task_3.html')
+
+def task_4(request):
+    return render(request, 'autoservice/task_4.html')
+
+
+# def sql (req, returnid = False):
+#     import sqlite3 as sqlite
+#     connection = sqlite.connect('db.sqlite3') # Название БД рядом с файликом manage.py
+#     cursor = connection.cursor()
+#     cursor.execute (str (req))
+#     connection.commit()
+#     if returnid == False:
+#         rows = cursor.fetchall ()
+#     else:
+#         rows = cursor.lastrowid
+#     cursor.close()
+#     connection.close()
+#     return rows
